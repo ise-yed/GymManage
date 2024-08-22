4026a523655ee124ebd99b2cfecffce1deb2d514
ise-yed <seyedmorteza2770@gmail.com>
2024-08-22 16:23:28 +0330

    create user model

---
+++ lib/data/models/user.dart
@@ -0,0 +1,18 @@
+import 'package:hive/hive.dart';
+
+class UserModel extends HiveObject {
+  @HiveField(0)
+  String name;
+  @HiveField(1)
+  String number;
+  @HiveField(2)
+  String registerDate;
+  @HiveField(3)
+  String expireDate;
+  // @HiveField(4)
+  UserModel(
+      {required this.expireDate,
+      required this.registerDate,
+      required this.name,
+      required this.number});
+}
